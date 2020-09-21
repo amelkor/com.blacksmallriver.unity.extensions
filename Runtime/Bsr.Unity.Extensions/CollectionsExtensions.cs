e915fb4baa803200f71ecd4402e35624181de3ab
amelkor <egoresurgo@live.ru>
2020-09-22 01:26:17 +0300

    Add collections extensions

--- Runtime/Bsr.Unity.Extensions/CollectionsExtensions.cs
+++ Runtime/Bsr.Unity.Extensions/CollectionsExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace Bsr.Unity.Extensions
 {
@@ -8,8 +9,26 @@ namespace Bsr.Unity.Extensions
         {
             if (dict.TryGetValue(key, out var value))
                 return value;
-            
+
             throw new KeyNotFoundException();
         }
+    }
+
+    public static void Add<TKey, TValue>(this IDictionary<TKey, IList> dictionary, (TKey key, TValue value) tuple) where TValue : class
+    {
+        var (key, value) = tuple;
+
+        if (value == null)
+            return;
+
+        dictionary[key].Add(value);
+    }
+
+    public static void ToArray<T>(this IList list, ref T[] array)
+    {
+        if (array == null)
+            array = new T[list.Count];
+
+        list.CopyTo(array, 0);
     }
 }
