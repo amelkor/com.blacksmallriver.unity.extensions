3b4e0a022aa0152555932ef426b1a793909c72d1
amelkor <30626539+amelkor@users.noreply.github.com>
2021-05-20 17:49:04 +0300

    add bounds to cells add Odin inspector

---
+++ Runtime/Bsr.Unity.Extensions/Unity/BoundsExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Bsr.Unity.Extensions.Unity
+{
+    public static class BoundsExtensions
+    {
+        public static bool Contains(this BoundsInt bounds, Vector2Int position)
+        {
+            return bounds.Contains(new Vector3Int(position.x, position.y, 0));
+        }
+    }
+}
